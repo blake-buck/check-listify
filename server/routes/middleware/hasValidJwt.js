a6eacc153daafe1422bce928cc69566ce7750261
blake-buck <blakemanbuck@gmail.com>
2020-04-02 14:24:11 -0500

    added comments to hasValidJwt

--- server/routes/middleware/hasValidJwt.js
+++ server/routes/middleware/hasValidJwt.js
@@ -2,6 +2,8 @@ const jsonwebtoken = require('jsonwebtoken');
 const jwkToPem = require('jwk-to-pem');
 const jwk = require('../../jwks.json');
 
+// this verifyJWT flow follows the one layed out in the documentation
+// reads a Json Web Key from jwks.json in root, and verifies it agains the token passed in
 function verifyJWT(token){
     const pem = jwkToPem(jwk);
     return new Promise((resolve, reject) => {
@@ -16,18 +18,22 @@ function verifyJWT(token){
     })
 }
 
+// the actual middleware used
 async function hasValidJwt(req, res, next){
     const { jwt } = req.headers;
 
+    // if no json web token is in request header, send a response
     if(!jwt){
         res.status(400).send('No JSON Web Token in request header');
     }
 
     try{
+        // if json web token is valid, continue
         await verifyJWT(jwt);
         next();
     }
     catch(e){
+        // if json web token is invalid, send a response
         res.status(400).send('JSON Web Token is not valid.')
     }
     
