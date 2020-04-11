9c420474ed9a71a9538abf4e238d1168c6145d95
blake-buck <blakemanbuck@gmail.com>
2020-04-11 16:10:09 -0500

    added list transitions to UserChecklists

--- client/src/components/UserChecklists/UserChecklists.vue
+++ client/src/components/UserChecklists/UserChecklists.vue
@@ -6,25 +6,41 @@
         </header>
         
         <div class='list-items'>
-            <ListItem v-for='checklist in pinnedChecklists' :key='checklist.Id' v-bind:checklist='checklist'></ListItem>
-            <ListItem v-for='checklist in checklists' :key='checklist.Id' v-bind:checklist='checklist'></ListItem>
+            <transition-group name='list'>
+                <ListItem v-for='checklist in pinnedChecklists' :key='checklist.Id' v-bind:checklist='checklist'></ListItem>
+                <ListItem v-for='checklist in checklists' :key='checklist.Id' v-bind:checklist='checklist'></ListItem>
+            </transition-group>
+            
             
             <transition name='fade'>
                 <block-input :shouldAutofocus='true' v-if='displayChecklistTemplate' :blurHandler='blurChecklist' :keyupHandler='keyupChecklist' placeholder='Checklist title'></block-input>
             </transition>
         </div>
-        <block-fab v-if='!displayChecklistTemplate' :clickHandler='showChecklistTemplate' displayText='Add Checklist'></block-fab>
+        <transition name='fade'>
+            <block-fab v-if='!displayChecklistTemplate' :clickHandler='showChecklistTemplate' displayText='Add Checklist'></block-fab>
+        </transition>
     </div>
 </template>
 
 <style scoped>
 
+    .list-enter-active {
+        transition: opacity 0.5s;
+    }
+    .list-leave-active{
+        transition:all 0.5s;
+        transform:translateX(60px);
+    }
+    .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
+
     .fade-enter-active{
         transition: opacity .2s;
-        }
+    }
     .fade-enter{
         opacity: 0;
-        }
+    }
     @keyframes fadeIn{
         from {opacity: 0.1;}
         to   {opacity: 1;}
