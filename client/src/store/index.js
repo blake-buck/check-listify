6848b24822fdea96c8c36b5448682906e74d3f5a
blake-buck <blakemanbuck@gmail.com>
2020-04-17 08:19:27 -0500

    Account config is synced with database

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -21,8 +21,6 @@ const store = new Vuex.Store({
 function syncWithDatabase(){
     // When syncing with the database
 
-    // retrieve new checklists and items from database
-
     // retrieve Local constants
     const localConstants = storageService.retrieveConstants();
 
@@ -31,41 +29,45 @@ function syncWithDatabase(){
     let deletedChecklists = localConstants.deletedChecklists ? [...localConstants.deletedChecklists] : [];
     deletedChecklists.forEach(id => {
         store.dispatch(constants.DELETE_CHECKLIST, id);
-    })
+    });
 
     // take "updatedChecklists" from localStorage and send put requests for each of them
     let updatedChecklists = localConstants.updatedChecklists ? [...localConstants.updatedChecklists] : [];
     updatedChecklists.forEach(checklist => {
         store.dispatch(constants.UPDATE_CHECKLIST, checklist);
-    })
+    });
 
     // take "createdChecklists" from localStorage and dispatch ADD_CHECKLIST for each of them
     let createdChecklists = localConstants.createdChecklists ? [...localConstants.createdChecklists] : [];
     createdChecklists.forEach(checklist => {
         store.dispatch(constants.ADD_CHECKLIST, checklist.Title);
         store.dispatch(constants.DELETE_CHECKLIST, checklist.Id)
-    })
+    });
 
     // take "deletedChecklistItems" from localStorage and send delete requests for each of them
     let deletedChecklistItems = localConstants.deletedChecklistItems ? [...localConstants.deletedChecklistItems] : [];
     deletedChecklistItems.forEach(id => {
         store.dispatch(constants.DELETE_CHECKLIST_ITEM, id);
-    })
+    });
 
     // take "updatedChecklistItems" from localStorage and send put requests for each of them
     let updatedChecklistItems = localConstants.updatedChecklistItems ? [...localConstants.updatedChecklistItems] : [];
     updatedChecklistItems.forEach(item => {
         store.dispatch(constants.UPDATE_CHECKLIST_ITEM, item);
-    })
+    });
 
     // take "createdChecklistItems" from localStorage and send post requests for each of them
     let createdChecklistItems = localConstants.createdChecklistItems ? [...localConstants.createdChecklistItems] : [];
     createdChecklistItems.forEach(item => {
         store.dispatch(constants.ADD_CHECKLIST_ITEM,{name: item.Name, checklistId: item.ChecklistId});
         store.dispatch(constants.DELETE_CHECKLIST_ITEM, item.Id)
-    })
+    });
 
     // take accountConfig from stored state and send put request for it -- local account settings always beat out stored account settings
+    store.dispatch(constants.UPDATE_ACCOUNT_CONFIG, storageService.retrieveAccountConfig());
+
+    // retrieve new checklists and items from database
+
 
     storageService.clearConstants();
 }
